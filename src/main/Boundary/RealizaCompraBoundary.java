345d724dc81fe1e69dc61c5e3492e51edbc241fe
João Dallapé <142693345+Zearus@users.noreply.github.com>
2024-05-28 16:16:03 -0300

    Update RealizaCompraBoundary.java

--- src/main/Boundary/RealizaCompraBoundary.java
+++ src/main/Boundary/RealizaCompraBoundary.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import Entities.Cliente;
 import Control.RealizaCompraControl;
 import Entities.Estoque;
-import Entities.Pedido;
 
 public class RealizaCompraBoundary {
     public RealizaCompraBoundary() {}

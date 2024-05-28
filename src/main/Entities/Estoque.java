0a60c121b74266d92be26471580127393dfce8b9
João Dallapé <142693345+Zearus@users.noreply.github.com>
2024-05-28 16:12:52 -0300

    Update Estoque.java

--- src/main/Entities/Estoque.java
+++ src/main/Entities/Estoque.java
@@ -30,6 +30,38 @@ public class Estoque {
         }
     }
 
+    public void listarEstoqueValores() {
+        System.out.println("Estoque:");
+        for (String produto : estoque.keySet()) {
+            String valor; 
+            int id;
+            if (produto.equalsIgnoreCase("Ração")) {
+                valor = "25";
+                id = 1;
+            } else if (produto.equalsIgnoreCase("Remédio")) {
+                valor = "15";
+                id = 2;
+            } else if (produto.equalsIgnoreCase("Brinquedo")) {
+                valor = "50";
+                id = 3;
+            } else if (produto.equalsIgnoreCase("Cama")) {
+                valor = "100";
+                id = 4;
+            } else if (produto.equalsIgnoreCase("Coleira")) {
+                valor = "30";
+                id = 5;
+            } else {
+                id = -1;
+                valor = "Produto indisponível para compra";
+            }
+            System.out.println(id + " - " + produto + " - preço: " + valor + " - quantidade no estoque: " + estoque.get(produto));
+        }
+    }
+
+    public HashMap<String, Integer> getEstoque() {
+        return  this.estoque;
+    }
+
     public void listarEstoque() {
         System.out.println("Estoque:");
         for (String produto : estoque.keySet()) {
